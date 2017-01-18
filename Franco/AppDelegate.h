223dca89fbd7e9cb77f40e7ef20d62931cefa476
Kate Owens <kowens711@gmail.com>
2017-01-18 13:38:29 -0500

    Initial Commit

---
+++ Franco/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Franco
+//
+//  Created by Katherine Owens on 1/18/17.
+//  Copyright © 2017 Katherine Owens. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Franco/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Franco
+//
+//  Created by Katherine Owens on 1/18/17.
+//  Copyright © 2017 Katherine Owens. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
